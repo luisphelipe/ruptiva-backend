6097937a16de0a4b632cb9c3b94d484ee4c110f7
Luis Phelipe <scphelipe@gmail.com>
2020-09-20 03:14:26 -0300

    Clear debug console.log

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,7 @@
 import path from "path";
 import dotenv from "dotenv";
 let test_env = process.env.NODE_ENV || "test";
-console.log("env before", process.env.NODE_ENV);
 dotenv.config({ path: path.resolve(process.cwd(), `.env-${test_env}`) });
-console.log("env after", process.env.NODE_ENV);
 
 import models from "../models";
 
